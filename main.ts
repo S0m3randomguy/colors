78426cd870c57b67ff28b4e69414f7249f64934e
S0m3randomguy <63020240+S0m3randomguy@users.noreply.github.com>
2022-07-11 20:47:37 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+namespace color {
+    export const transparent    = 0
+    export const white          = 1
+    export const red            = 2
+    export const pink           = 3
+    export const orange         = 4
+    export const yellow         = 5
+    export const cyan           = 6
+    export const green          = 7
+    export const blue           = 8
+    export const light_blue     = 9
+    export const purple         = 10
+    export const light_purple   = 11
+    export const dark_purple    = 12
+    export const tan            = 13
+    export const brown          = 14
+    export const black          = 15
+}
